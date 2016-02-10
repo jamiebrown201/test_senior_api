52123b605a621126e7a13159c8cb87b6ed9eb47e
Jamie Brown <jamiebrown201@hotmail.com>
2016-02-10 12:02:18 +0000

    initialize

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', process.env.PORT || 3500);
 
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
 
 app.listen(app.get('port'), function() {
     console.log('Server up: http://localhost:' + app.get('port'));
+});
+
+
+
+app.get('/', function(req, res) {
+  res.send('hello world');
 });
