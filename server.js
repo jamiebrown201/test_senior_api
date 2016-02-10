4cc87a1e5fcc27e11ff8813baef9aa719ef9f7d6
allimac <camillacolser@hotmail.com>
2016-02-10 11:48:21 +0000

    basic express server up

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', process.env.PORT || 3500);
 
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
 
-var router = new express.Router();
-
-
-
-
-
-app.use('/', router);
-
-var server = app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function() {
     console.log('Server up: http://localhost:' + app.get('port'));
 });
